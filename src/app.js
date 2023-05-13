0dbd62cb6c60b24c09d6ee8ab1acef4e588804db
Maksim1509 <maaa23@mail.ru>
2023-05-13 21:03:07 +0400

    refactor: изменение ковычек на одиночные в App.js

--- src/app.js
+++ src/app.js
@@ -11,29 +11,29 @@ function App({ store }) {
   const list = store.getState().list;
 
   return (
-    <div className="App">
-      <div className="App-head">
+    <div className='App'>
+      <div className='App-head'>
         <h1>Приложение на чистом JS</h1>
       </div>
-      <div className="App-controls">
+      <div className='App-controls'>
         <button onClick={() => store.addItem()}>Добавить</button>
       </div>
-      <div className="App-center">
-        <div className="List">
+      <div className='App-center'>
+        <div className='List'>
           {list.map((item) => (
-            <div key={item.code} className="List-item">
+            <div key={item.code} className='List-item'>
               <div
                 className={'Item' + (item.selected ? ' Item_selected' : '')}
                 onClick={() => store.selectItem(item.code)}
               >
-                <div className="Item-code">{item.code}</div>
-                <div className="Item-title">
+                <div className='Item-code'>{item.code}</div>
+                <div className='Item-title'>
                   {item.title}
                   {item.numberOfSelected &&
                     `  |  Выделяли ${item.numberOfSelected} раз`}
                 </div>
 
-                <div className="Item-actions">
+                <div className='Item-actions'>
                   <button onClick={(e) => store.deleteItem(item.code, e)}>
                     Удалить
                   </button>
